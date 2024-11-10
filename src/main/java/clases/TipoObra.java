41055f9493d8a5d7f35d9c9a333ed83d083003c5
Bartoluche <Bartoluche@DESKTOP-BVQ0VA2>
2024-11-09 23:36:08 -0300

    Creacion de la clase TipoObra

---
+++ src/main/java/clases/TipoObra.java
@@ -0,0 +1,38 @@
+package clases;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TipoObra implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private int Id_tipo_obra;
+    
+    private String descripcion;
+
+    public TipoObra() {
+    }
+
+    public int getId_tipo_obra() {
+        return Id_tipo_obra;
+    }
+
+    public void setId_tipo_obra(int Id_tipo_obra) {
+        this.Id_tipo_obra = Id_tipo_obra;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+}
