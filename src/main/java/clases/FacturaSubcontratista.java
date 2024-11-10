66e92cf02d20a135da98c59215c9d2f7eb978a6f
Bartoluche <Bartoluche@DESKTOP-BVQ0VA2>
2024-11-10 19:35:00 -0300

    Creando la clase "FacturaSubcontratista"

---
+++ src/main/java/clases/FacturaSubcontratista.java
@@ -0,0 +1,60 @@
+package clases;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class FacturaSubcontratista implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private int id_subcontratista;
+    
+    private int numero_factura;
+    private LocalDate fecha_emision;
+    private BigDecimal monto;
+
+    public FacturaSubcontratista() {
+    }
+
+    public int getId_subcontratista() {
+        return id_subcontratista;
+    }
+
+    public void setId_subcontratista(int id_subcontratista) {
+        this.id_subcontratista = id_subcontratista;
+    }
+
+    public int getNumero_factura() {
+        return numero_factura;
+    }
+
+    public void setNumero_factura(int numero_factura) {
+        this.numero_factura = numero_factura;
+    }
+
+    public LocalDate getFecha_emision() {
+        return fecha_emision;
+    }
+
+    public void setFecha_emision(LocalDate fecha_emision) {
+        this.fecha_emision = fecha_emision;
+    }
+
+    public BigDecimal getMonto() {
+        return monto;
+    }
+
+    public void setMonto(BigDecimal monto) {
+        this.monto = monto;
+    }
+    
+    
+}
