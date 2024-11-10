80d70c08ab2ae360019e97cb93aff9f77f79bf38
Bartoluche <Bartoluche@DESKTOP-BVQ0VA2>
2024-11-09 21:44:14 -0300

    Creacion de la clase "Sociedad"

---
+++ src/main/java/clases/Sociedad.java
@@ -0,0 +1,107 @@
+package clases;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Sociedad implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private int id_sociedad;
+    
+    //private Seguro seguro;
+    
+    private String cuit_sociedad;
+    private String razon_social;
+    private String telefono;
+    private String mail;
+    private String Calle;
+    private int altura;
+    private String localidad;
+    //private Provincia provincia;
+
+    public Sociedad() {
+    }
+
+    public Sociedad(String cuit_sociedad, String razon_social, String telefono, String mail, String Calle, int altura, String localidad) {
+        this.cuit_sociedad = cuit_sociedad;
+        this.razon_social = razon_social;
+        this.telefono = telefono;
+        this.mail = mail;
+        this.Calle = Calle;
+        this.altura = altura;
+        this.localidad = localidad;
+    }
+
+    public int getId_sociedad() {
+        return id_sociedad;
+    }
+
+    public void setId_sociedad(int id_sociedad) {
+        this.id_sociedad = id_sociedad;
+    }
+
+    public String getCuit_sociedad() {
+        return cuit_sociedad;
+    }
+
+    public void setCuit_sociedad(String cuit_sociedad) {
+        this.cuit_sociedad = cuit_sociedad;
+    }
+
+    public String getRazon_social() {
+        return razon_social;
+    }
+
+    public void setRazon_social(String razon_social) {
+        this.razon_social = razon_social;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getCalle() {
+        return Calle;
+    }
+
+    public void setCalle(String Calle) {
+        this.Calle = Calle;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+    
+    
+}
