4f314160402bf2341e7b21646bb9ce3b4d8bce46
Bartoluche <Bartoluche@DESKTOP-BVQ0VA2>
2024-11-11 19:54:22 -0300

    Creando la clase "rubro", la cual sera item y sub item de la clase "Presupuesto"

---
+++ src/main/java/clases/Rubro.java
@@ -0,0 +1,116 @@
+package clases;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Rubro implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private int id_rubro;
+    
+    private String nombre;
+    
+    
+    
+    @Column(precision = 20, scale = 10)
+    private BigDecimal presupuesto;
+    
+    @Column(precision = 20, scale = 10)
+    private BigDecimal adelanto;
+    
+    @Column(precision = 20, scale = 10)
+    private BigDecimal saldoDescuento;
+    
+    @Column(precision = 20, scale = 10)
+    private BigDecimal presupuestoCAC;
+    
+    
+    // float para porcentajes chicos
+    private float porcentajeAvance;
+    
+    @Column(precision = 20, scale = 10)
+    private BigDecimal cert1;
+
+    public Rubro() {
+    }
+
+    public Rubro(String nombre, BigDecimal presupuesto) {
+        this.nombre = nombre;
+        this.presupuesto = presupuesto;
+    }
+
+    public int getId_rubro() {
+        return id_rubro;
+    }
+
+    public void setId_rubro(int id_rubro) {
+        this.id_rubro = id_rubro;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(BigDecimal presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public BigDecimal getAdelanto() {
+        return adelanto;
+    }
+
+    public void setAdelanto(BigDecimal adelanto) {
+        this.adelanto = adelanto;
+    }
+
+    public BigDecimal getSaldoDescuento() {
+        return saldoDescuento;
+    }
+
+    public void setSaldoDescuento(BigDecimal saldoDescuento) {
+        this.saldoDescuento = saldoDescuento;
+    }
+
+    public BigDecimal getPresupuestoCAC() {
+        return presupuestoCAC;
+    }
+
+    public void setPresupuestoCAC(BigDecimal presupuestoCAC) {
+        this.presupuestoCAC = presupuestoCAC;
+    }
+
+    public float getPorcentajeAvance() {
+        return porcentajeAvance;
+    }
+
+    public void setPorcentajeAvance(float porcentajeAvance) {
+        this.porcentajeAvance = porcentajeAvance;
+    }
+
+    public BigDecimal getCert1() {
+        return cert1;
+    }
+
+    public void setCert1(BigDecimal cert1) {
+        this.cert1 = cert1;
+    }
+
+    
+}
